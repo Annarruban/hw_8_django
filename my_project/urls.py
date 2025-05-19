339952e0b5cabed2dfeb69be040fbd0154ebe3de
Annarruban <anna14146@ukr.net>
2025-05-19 21:09:15 +0200

    Task 3

--- my_project/urls.py
+++ my_project/urls.py
@@ -19,6 +19,10 @@ from django.urls import path
 from django.urls.conf import include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 
 from app import views
 from app.views.category import CategoryViewSet
@@ -27,6 +31,19 @@ from app.views.subtask import SubtaskRetrieveUpdateDestroyView, SubtaskListCreat
 
 router = DefaultRouter()
 router.register(r'category', CategoryViewSet)
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="First API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@local.com"),
+        license=openapi.License(name="BSD License"),
+        ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+    )
 
 urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
@@ -41,5 +58,9 @@ urlpatterns = [
     path('subtask/', SubtaskListCreateView.as_view(), name='subtask-list-create'),
     path('subtask/<int:pk>/', SubtaskRetrieveUpdateDestroyView.as_view(),
          name='subtask-detail-update-delete'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0),
+         name='schema-redoc'),
     path('', include(router.urls)),
 ]
